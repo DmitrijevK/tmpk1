729e834fd7da75ed51a13bbc9577d5b14710d019
kirill <kirill@DESKTOP-D4NV2EJ>
2021-04-15 23:59:43 +0300

    111

---
+++ tmpk1/tmpk1/stranitsi/teacher.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tmpk1.stranitsi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class teacher : TabbedPage
+    {
+        public teacher()
+        {
+            InitializeComponent();
+        }
+    }
+}
