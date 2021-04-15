1511162b9d32e44f611e0e0facfed0889fa5bcff
kirill <kirill@DESKTOP-D4NV2EJ>
2021-04-16 02:11:30 +0300

    ddd

---
+++ tmpk1/tmpk1/classs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tmpk1
+{
+        public class classs
+        {
+            public string Ltext { get; set; }
+
+            public string lDetail { get; set; }
+
+            public string Limage { get; set; }
+
+        }
+}
--- tmpk1/tmpk1/stranitsi/teacher.xaml.cs
+++ tmpk1/tmpk1/stranitsi/teacher.xaml.cs
@@ -15,6 +15,47 @@ namespace tmpk1.stranitsi
         public teacher()
         {
             InitializeComponent();
+            listik.ItemsSource = Menulist();
+        }
+
+        private List<classs> Menulist()
+        {
+            var uchetelja = new List<classs>();
+            uchetelja.Add(new classs()
+            {
+                Ltext = "Natalja Antonova",
+                Limage = "Antonova",
+                lDetail = "Natalia.Antonova@mahtra.edu.ee"
+            });
+            uchetelja.Add(new classs()
+            {
+                Ltext = "Anna Sulipa",
+                Limage = "Sulipa",
+                lDetail = "Anna.Sulipa@mahtra.edu.ee"
+            });
+            uchetelja.Add(new classs()
+            {
+                Ltext = "Liudmila Kuzichkina",
+                Limage = "Kuzichkina",
+                lDetail = "Liudmila.Kuzichkina@mahtra.edu.ee"
+            });
+            uchetelja.Add(new classs()
+            {
+                Ltext = "Olga Jermolajeva",
+                Limage = "Jermolajeva",
+                lDetail = "Olga.Jermolajeva@mahtra.edu.ee"
+            });
+            uchetelja.Add(new classs()
+            {
+                Ltext = "Natalia Hort",
+                Limage = "Hort",
+                lDetail = "Natalia.Hort@mahtra.edu.ee"
+            });
+            return uchetelja;
+        }
+
+        private void listik_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
         }
     }
 }
