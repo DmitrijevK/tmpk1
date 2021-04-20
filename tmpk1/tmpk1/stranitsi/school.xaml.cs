0c9019b038f0c4d79747386b6dbaa5afdebc3636
kirill <kirill@DESKTOP-D4NV2EJ>
2021-04-21 02:18:51 +0300

    ddd

--- tmpk1/tmpk1/stranitsi/school.xaml.cs
+++ tmpk1/tmpk1/stranitsi/school.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,11 @@ namespace tmpk1.stranitsi
         public school()
         {
             InitializeComponent();
+        }
+
+        private async void svjaz_Clicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync(new Uri("https://ru.stackoverflow.com"));
         }
     }
 }
