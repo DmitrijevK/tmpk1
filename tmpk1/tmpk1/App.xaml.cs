856e1c0b4363973e7d6d68f500d3dcb4821cbd47
kirill <kirill@DESKTOP-D4NV2EJ>
2021-04-23 19:49:36 +0300

    12212

--- tmpk1/tmpk1/App.xaml.cs
+++ tmpk1/tmpk1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace tmpk1
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new vkl();
         }
 
         protected override void OnStart()
---
+++ tmpk1/tmpk1/vkl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tmpk1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class vkl : ContentPage
+    {
+        public vkl(string image)
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            LogoAnimation(image);
+        }
+
+        public async Task LogoAnimation(string image)
+        {
+            if (string.IsNullOrWhiteSpace(image))
+            {
+                imgLogo.Source = "logo.png";
+            }
+            else
+            {
+                imgLogo.Source = image;
+            }
+
+            imgLogo.Opacity = 0;
+            await imgLogo.FadeTo(1, 3000);
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
